33430ab634652a8fb268b0078b5453f1b5de7e08
kionn <k10nn@kali.k10nn>
2023-06-22 22:12:32 -0300

    car routes and crud

--- app/controllers/car_controller.py
+++ app/controllers/car_controller.py
@@ -1,10 +1,41 @@
 from sqlalchemy.orm import Session
 from sqlalchemy.exc import NoResultFound
-
 from ..models.car_model import Car
+from ..schemas.car_schema import CarCreate
 
 def get_car(db: Session, car_id: int):
     try:
         return db.query(Car).filter(Car.id == car_id).first()
     except NoResultFound:
         return None
+
+def get_all_cars(db: Session):
+    return db.query(Car).all()
+
+def create_car(car: CarCreate, db: Session):
+    db_car = Car(
+        maker=car.maker,
+        quantity=car.quantity,
+        year_birth=car.year_birth
+    )
+    db.add(db_car)
+    db.commit()
+    db.refresh(db_car)
+    return db_car
+
+def delete_car(car_id: int, db: Session):
+    db_car_del = db.query(Car).filter(Car.id == car_id).first()
+    if db_car_del:
+        db.delete(db_car_del)
+        db.commit()
+    return db_car_del
+
+def update_car(car: Car, car_id: int, db: Session):
+    db_car_upd = db.query(Car).filter(Car.id == car_id).first()
+    db_car_upd.maker=car.maker
+    db_car_upd.quantity=car.quantity
+    db_car_upd.year_birth=car.year_birth
+
+    db.commit()
+
+    return db_car_upd
--- app/routes/car_routes.py
+++ app/routes/car_routes.py
@@ -1,13 +1,36 @@
-from fastapi import APIRouter
-from ..controllers import car_controller
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
+from ..schemas import car_schema
+from ..controllers import car_controller as car_crud
+from ..data_base import get_db
 
 car_router = APIRouter()
 
+#GET ONE CAR
+@car_router.get("/cars/{car_id}", response_model=car_schema.Car)
+async def get_one_car(car_id: int, db: Session = Depends(get_db)):
+	return car_crud.get_car(db, car_id)
 
-@car_router.get("/cars")
-async def get_cars():
-    return car_controller.get_cars()
 
-@car_router.post("/cars")
-async def create_car(car):
-    return car_controller.create_car(car)
+#GET ALL CARS
+@car_router.get("/cars", response_model=list[car_schema.Car])
+def get_cars_all(db: Session = Depends(get_db)):
+	return get_cars_all(db)
+
+
+#CREATE ONE CAR
+@car_router.post("/cars", response_model=car_schema.Car)
+async def create_one_car(car: car_schema.Car, db: Session = Depends(get_db)):
+	return car_crud.create_car(car.dict(), db)
+
+
+#UPDATE ONE CAR
+@car_router.put("/cars/{car_id}", response_model=car_schema.Car)
+async def update_one_car(car: car_schema.Car, car_id: int, db: Session = Depends(get_db)):
+	return car_crud.update_car(car.dict(), car_id, db)
+
+
+#DELETE ONE CAR
+@car_router.delete("/cars/{car_id}", response_model=car_schema.Car)
+def delete_single_car(car_id: int, db: Session = Depends(get_db)):
+    return car_crud.delete_car(db, car_id)
