95d2c91532443ee329857e699f30de707372f6f6
kionn <k10nn@kali.k10nn>
2023-06-23 13:26:42 -0300

    load env script

--- app/routes/car_routes.py
+++ app/routes/car_routes.py
@@ -8,54 +8,37 @@ from ..data_base import get_db
 car_router = APIRouter()
 
 
-@car_router.get("/car/{car_id}", response_model=car_schema.Car)
+# GET ONE CAR
+@car_router.get("/car/id/{car_id}", response_model=car_schema.Car)
 def get_cars(car_id: int, db: Session = Depends(get_db)):
     return car_crud.get_car(car_id=car_id, db=db)
 
+
+# GET ALL CARS
 @car_router.get("/cars", response_model=List[car_schema.Car])
 def get_cars(db: Session = Depends(get_db)):
     return car_crud.get_cars(db=db)
 
 
+# GET CAR BY MAKER
+@car_router.get("/car/maker/{maker}", response_model=car_schema.Car)
+def get_car_by_maker(maker: str, db: Session = Depends(get_db)):
+    return car_crud.get_car_by_maker(maker=maker, db=db)
+
+
+# CREATE CAR
 @car_router.post("/car", response_model=car_schema.Car)
 def create_car(car: car_schema.CarCreate, db: Session = Depends(get_db)):
     return car_crud.create_car(car=car, db=db)
 
 
-@car_router.delete("/car/{car_id}", response_model=car_schema.Car)
-def delete_car(car_id: int, db: Session = Depends(get_db)):
-    return car_crud.delete_car(car_id=car_id, db=db)
-
+# UPDATE CAR
 @car_router.put("/car/{car_id}", response_model=car_schema.Car)
 def update_car(car_id: int, car: car_schema.CarCreate, db: Session = Depends(get_db)):
     return car_crud.update_car(car_id=car_id, car=car, db=db)
 
 
-# #GET ONE CAR
-# @car_router.get("/cars/{car_id}", response_model=car_schema.Car)
-# async def get_one_car(car_id: int, db: Session = Depends(get_db)):
-# 	return car_crud.get_car(db, car_id)
-
-
-# #GET ALL CARS
-# @car_router.get("/cars", response_model=list[car_schema.Car])
-# def get_cars_all(db: Session = Depends(get_db)):
-# 	return get_cars_all(db)
-
-
-# #CREATE ONE CAR
-# @car_router.post("/cars", response_model=car_schema.Car)
-# async def create_one_car(car: car_schema.Car, db: Session = Depends(get_db)):
-# 	return car_crud.create_car(car.dict(), db)
-
-
-# #UPDATE ONE CAR
-# @car_router.put("/cars/{car_id}", response_model=car_schema.Car)
-# async def update_one_car(car: car_schema.Car, car_id: int, db: Session = Depends(get_db)):
-# 	return car_crud.update_car(car.dict(), car_id, db)
-
-
-# #DELETE ONE CAR
-# @car_router.delete("/cars/{car_id}", response_model=car_schema.Car)
-# def delete_single_car(car_id: int, db: Session = Depends(get_db)):
-#     return car_crud.delete_car(db, car_id)
+# DELETE CAR
+@car_router.delete("/car/{car_id}", response_model=car_schema.Car)
+def delete_car(car_id: int, db: Session = Depends(get_db)):
+    return car_crud.delete_car(car_id=car_id, db=db)
--- script.py
+++ script.py
@@ -1,72 +1,79 @@
+from dotenv import load_dotenv
 import psycopg2
+import os
 
-# Define your data to be inserted into the database
+load_dotenv()
+
+POSTGRES_DB = os.getenv("POSTGRES_DB")
+POSTGRES_USER = os.getenv("POSTGRES_USER")
+POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
+
 data = [
-    {"maker": "Acura", "quantity": 55, "year_birth": 1986},
-    {"maker": "Alfa Romeo", "quantity": 17, "year_birth": 1910},
-    {"maker": "Alpheon", "quantity": 1, "year_birth": 2010},
-    {"maker": "Audi", "quantity": 93, "year_birth": 1909},
-    {"maker": "AVGM", "quantity": 3, "year_birth": 2001},
-    {"maker": "Bentley", "quantity": 20, "year_birth": 1919},
-    {"maker": "BMW", "quantity": 158, "year_birth": 1916},
-    {"maker": "Buick", "quantity": 219, "year_birth": 1903},
-    {"maker": "Cadillac", "quantity": 180, "year_birth": 1902},
-    {"maker": "Chevrolet", "quantity": 515, "year_birth": 1911},
-    {"maker": "Chrysler", "quantity": 56, "year_birth": 1925},
-    {"maker": "Daewoo", "quantity": 6, "year_birth": 1937},
-    {"maker": "Dodge", "quantity": 119, "year_birth": 1900},
-    {"maker": "Ferrari", "quantity": 7, "year_birth": 1939},
-    {"maker": "Fiat", "quantity": 30, "year_birth": 1899},
-    {"maker": "Ford", "quantity": 297, "year_birth": 1903},
-    {"maker": "Freightliner", "quantity": 2, "year_birth": 1942},
-    {"maker": "Geo", "quantity": 5, "year_birth": 1989},
-    {"maker": "GM", "quantity": 3, "year_birth": 1908},
-    {"maker": "GMC", "quantity": 173, "year_birth": 1911},
-    {"maker": "Holden", "quantity": 183, "year_birth": 1856},
-    {"maker": "Honda", "quantity": 97, "year_birth": 1948},
-    {"maker": "HSV", "quantity": 44, "year_birth": 1987},
-    {"maker": "Hummer", "quantity": 13, "year_birth": 1992},
-    {"maker": "Infiniti", "quantity": 60, "year_birth": 1989},
-    {"maker": "Isuzu", "quantity": 14, "year_birth": 1916},
-    {"maker": "Jeep", "quantity": 89, "year_birth": 1941},
-    {"maker": "Karma", "quantity": 3, "year_birth": 2014},
-    {"maker": "Lamborghini", "quantity": 17, "year_birth": 1963},
-    {"maker": "Lancia", "quantity": 10, "year_birth": 1906},
-    {"maker": "Lexus", "quantity": 18, "year_birth": 1989},
-    {"maker": "Lincoln", "quantity": 94, "year_birth": 1917},
-    {"maker": "Maserati", "quantity": 26, "year_birth": 1914},
-    {"maker": "Mazda", "quantity": 96, "year_birth": 1920},
-    {"maker": "Mercedes-Benz", "quantity": 138, "year_birth": 1926},
-    {"maker": "Mercury", "quantity": 31, "year_birth": 1938},
-    {"maker": "MINI", "quantity": 41, "year_birth": 1959},
-    {"maker": "Mitsubishi", "quantity": 41, "year_birth": 1870},
-    {"maker": "Nissan", "quantity": 157, "year_birth": 1933},
-    {"maker": "Oldsmobile", "quantity": 54, "year_birth": 1897},
-    {"maker": "Opel", "quantity": 6, "year_birth": 1862},
-    {"maker": "Pagani", "quantity": 2, "year_birth": 1992},
-    {"maker": "Pontiac", "quantity": 122, "year_birth": 1926},
-    {"maker": "Porsche", "quantity": 8, "year_birth": 1931},
-    {"maker": "RAM", "quantity": 37, "year_birth": 2010},
-    {"maker": "Rolls-Royce", "quantity": 33, "year_birth": 1904},
-    {"maker": "Saab", "quantity": 9, "year_birth": 1945},
-    {"maker": "Saturn", "quantity": 36, "year_birth": 1985},
-    {"maker": "Scion", "quantity": 8, "year_birth": 2003},
-    {"maker": "Smart", "quantity": 6, "year_birth": 1994},
-    {"maker": "SRT", "quantity": 3, "year_birth": 1989},
-    {"maker": "Sterling", "quantity": 2, "year_birth": 1997},
-    {"maker": "Subaru", "quantity": 75, "year_birth": 1953},
-    {"maker": "Suzuki", "quantity": 28, "year_birth": 1909},
-    {"maker": "Toyota", "quantity": 121, "year_birth": 1937},
-    {"maker": "Volkswagen", "quantity": 151, "year_birth": 1937},
-    {"maker": "Volvo", "quantity": 47, "year_birth": 1927}
+	{"maker": "Acura", "quantity": 55, "year_birth": 1986},
+	{"maker": "Alfa Romeo", "quantity": 17, "year_birth": 1910},
+	{"maker": "Alpheon", "quantity": 1, "year_birth": 2010},
+	{"maker": "Audi", "quantity": 93, "year_birth": 1909},
+	{"maker": "AVGM", "quantity": 3, "year_birth": 2001},
+	{"maker": "Bentley", "quantity": 20, "year_birth": 1919},
+	{"maker": "BMW", "quantity": 158, "year_birth": 1916},
+	{"maker": "Buick", "quantity": 219, "year_birth": 1903},
+	{"maker": "Cadillac", "quantity": 180, "year_birth": 1902},
+	{"maker": "Chevrolet", "quantity": 515, "year_birth": 1911},
+	{"maker": "Chrysler", "quantity": 56, "year_birth": 1925},
+	{"maker": "Daewoo", "quantity": 6, "year_birth": 1937},
+	{"maker": "Dodge", "quantity": 119, "year_birth": 1900},
+	{"maker": "Ferrari", "quantity": 7, "year_birth": 1939},
+	{"maker": "Fiat", "quantity": 30, "year_birth": 1899},
+	{"maker": "Ford", "quantity": 297, "year_birth": 1903},
+	{"maker": "Freightliner", "quantity": 2, "year_birth": 1942},
+	{"maker": "Geo", "quantity": 5, "year_birth": 1989},
+	{"maker": "GM", "quantity": 3, "year_birth": 1908},
+	{"maker": "GMC", "quantity": 173, "year_birth": 1911},
+	{"maker": "Holden", "quantity": 183, "year_birth": 1856},
+	{"maker": "Honda", "quantity": 97, "year_birth": 1948},
+	{"maker": "HSV", "quantity": 44, "year_birth": 1987},
+	{"maker": "Hummer", "quantity": 13, "year_birth": 1992},
+	{"maker": "Infiniti", "quantity": 60, "year_birth": 1989},
+	{"maker": "Isuzu", "quantity": 14, "year_birth": 1916},
+	{"maker": "Jeep", "quantity": 89, "year_birth": 1941},
+	{"maker": "Karma", "quantity": 3, "year_birth": 2014},
+	{"maker": "Lamborghini", "quantity": 17, "year_birth": 1963},
+	{"maker": "Lancia", "quantity": 10, "year_birth": 1906},
+	{"maker": "Lexus", "quantity": 18, "year_birth": 1989},
+	{"maker": "Lincoln", "quantity": 94, "year_birth": 1917},
+	{"maker": "Maserati", "quantity": 26, "year_birth": 1914},
+	{"maker": "Mazda", "quantity": 96, "year_birth": 1920},
+	{"maker": "Mercedes-Benz", "quantity": 138, "year_birth": 1926},
+	{"maker": "Mercury", "quantity": 31, "year_birth": 1938},
+	{"maker": "MINI", "quantity": 41, "year_birth": 1959},
+	{"maker": "Mitsubishi", "quantity": 41, "year_birth": 1870},
+	{"maker": "Nissan", "quantity": 157, "year_birth": 1933},
+	{"maker": "Oldsmobile", "quantity": 54, "year_birth": 1897},
+	{"maker": "Opel", "quantity": 6, "year_birth": 1862},
+	{"maker": "Pagani", "quantity": 2, "year_birth": 1992},
+	{"maker": "Pontiac", "quantity": 122, "year_birth": 1926},
+	{"maker": "Porsche", "quantity": 8, "year_birth": 1931},
+	{"maker": "RAM", "quantity": 37, "year_birth": 2010},
+	{"maker": "Rolls-Royce", "quantity": 33, "year_birth": 1904},
+	{"maker": "Saab", "quantity": 9, "year_birth": 1945},
+	{"maker": "Saturn", "quantity": 36, "year_birth": 1985},
+	{"maker": "Scion", "quantity": 8, "year_birth": 2003},
+	{"maker": "Smart", "quantity": 6, "year_birth": 1994},
+	{"maker": "SRT", "quantity": 3, "year_birth": 1989},
+	{"maker": "Sterling", "quantity": 2, "year_birth": 1997},
+	{"maker": "Subaru", "quantity": 75, "year_birth": 1953},
+	{"maker": "Suzuki", "quantity": 28, "year_birth": 1909},
+	{"maker": "Toyota", "quantity": 121, "year_birth": 1937},
+	{"maker": "Volkswagen", "quantity": 151, "year_birth": 1937},
+	{"maker": "Volvo", "quantity": 47, "year_birth": 1927}
 ]
 
 # Establish a connection to the PostgreSQL database
 conn = psycopg2.connect(
-    host="localhost",
-    database="db_api",
-    user="postgres",
-    password="password"
+	host="localhost",
+	database=POSTGRES_DB,
+	user=POSTGRES_USER,
+	password=POSTGRES_PASSWORD
 )
 
 # Create a cursor object to execute SQL queries
@@ -74,12 +81,12 @@ cursor = conn.cursor()
 
 # Iterate through your data and execute INSERT queries
 for item in data:
-    maker = item["maker"]
-    quantity = item["quantity"]
-    year_birth = item["year_birth"]
-    
-    query = f"INSERT INTO cars (maker, quantity, year_birth) VALUES ('{maker}', {quantity}, {year_birth})"
-    cursor.execute(query)
+	maker = item["maker"]
+	quantity = item["quantity"]
+	year_birth = item["year_birth"]
+	
+	query = f"INSERT INTO cars (maker, quantity, year_birth) VALUES ('{maker}', {quantity}, {year_birth})"
+	cursor.execute(query)
 
 # Commit the changes and close the connection
 conn.commit()
