c7c258d2866bf44e4d075bb9e7741e6f43c92f41
Lucas Pires <piresnogueiralucas@gmail.com>
2023-07-03 14:29:11 -0300

    done

--- backend/app/routes/car_routes.py
+++ backend/app/routes/car_routes.py
@@ -6,7 +6,7 @@ from ..schemas import car_schema
 from ..controllers import car_controller as car_crud
 from ..depends import get_session_current_db, verify_token
 
-car_router = APIRouter(dependencies=[Depends(verify_token)])
+car_router = APIRouter()
 
 
 # GET ONE CAR
@@ -28,18 +28,18 @@ def get_car_by_maker(maker: str, db: Session = Depends(get_session_current_db)):
 
 
 # CREATE CAR
-@car_router.post("/car", response_model=car_schema.Car)
+@car_router.post("/car", response_model=car_schema.Car, dependencies=[Depends(verify_token)])
 def create_car(car: car_schema.CarCreate, db: Session = Depends(get_session_current_db)):
     return car_crud.create_car(car=car, db=db)
 
 
 # UPDATE CAR
-@car_router.put("/car/{car_id}", response_model=car_schema.Car)
+@car_router.put("/car/{car_id}", response_model=car_schema.Car, dependencies=[Depends(verify_token)])
 def update_car(car_id: int, car: car_schema.CarCreate, db: Session = Depends(get_session_current_db)):
     return car_crud.update_car(car_id=car_id, car=car, db=db)
 
 
 # DELETE CAR
-@car_router.delete("/car/{car_id}", response_model=car_schema.Car)
+@car_router.delete("/car/{car_id}", response_model=car_schema.Car, dependencies=[Depends(verify_token)])
 def delete_car(car_id: int, db: Session = Depends(get_session_current_db)):
     return car_crud.delete_car(car_id=car_id, db=db)
